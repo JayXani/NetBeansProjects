2b8a14d176080a38bea077eb70313e633b2d0ec0
JayXani <112290478+JayXani@users.noreply.github.com>
2023-05-20 16:48:00 -0300

    Update project of OO

--- gdrive/src/gdrive/Gdrive.java
+++ gdrive/src/gdrive/Gdrive.java
@@ -1,4 +1,3 @@
-
 package gdrive;
 
 import java.util.ArrayList;
@@ -8,7 +7,7 @@ public class Gdrive {
 
     Scanner teclado = new Scanner(System.in);
     ArrayList<Pasta> novaPasta = new ArrayList();
-    
+
     public void adicionarPasta(Pasta adicionarPasta) {
         novaPasta.add(adicionarPasta);
     }
@@ -29,8 +28,33 @@ public class Gdrive {
 
     }
 
-    public void calcTamanhoPast() {
-
+    public double calcTamanhoPast() {
+        System.out.println("\nInforme o nome da pasta que deseja verificar o tamanho:");
+        String nomePastaTamanho = teclado.next();
+        double tamanhoPasta = 1;
+        if (novaPasta.isEmpty()) {
+            System.out.println("\nDeve-se primeiro criar uma pasta !");
+        } else {
+            for (Pasta verifyPasta : novaPasta) {
+                if (verifyPasta.nomePasta.equals(nomePastaTamanho)) {
+                    if (!verifyPasta.novoArquivo.isEmpty()) {
+                        for (Arquivo novoArquivo : verifyPasta.novoArquivo) {
+                            tamanhoPasta += novoArquivo.tamanhoArquivo;
+                        }
+                    }
+                    if (!verifyPasta.novaSubPasta.isEmpty()) {
+                        for (SubPastas novaSubPasta : verifyPasta.novaSubPasta) {
+                            if (!novaSubPasta.novoArquivo.isEmpty()) {
+                                for (Arquivo getTamanhoArquivo : novaSubPasta.novoArquivo) {
+                                    tamanhoPasta += getTamanhoArquivo.tamanhoArquivo;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return tamanhoPasta;
     }
 
     public void excluirPasta() {
@@ -39,30 +63,30 @@ public class Gdrive {
         String escolhaPastaRemove;
         boolean stop = true;
         System.out.println("\nEscolha uma das opcoes abaixo: \n");
-        
-        while(stop != false){
+
+        while (stop != false) {
             System.out.println(menu);
             escolhaExcluir = teclado.next();
-            switch(escolhaExcluir){
+            switch (escolhaExcluir) {
                 case "1":
-                    if(novaPasta.isEmpty()){
+                    if (novaPasta.isEmpty()) {
                         System.out.println("\nNao eh possivel excluir uma pasta inexistente !\n");
                         stop = false;
-                    }else{
+                    } else {
                         System.out.println("\nEscolha uma pasta para exclusao !:\n");
                         pesquisarPasta();
                         escolhaPastaRemove = teclado.next();
-                        for(int i = 0; i < novaPasta.size(); i++){
-                            if(novaPasta.get(i).nomePasta.equals(escolhaPastaRemove)){
+                        for (int i = 0; i < novaPasta.size(); i++) {
+                            if (novaPasta.get(i).nomePasta.equals(escolhaPastaRemove)) {
                                 System.out.println("Pasta encontrada: " + novaPasta.get(i).nomePasta);
                                 System.out.println("\nDeseja excluir a pasta ? Y/N");
                                 escolhaPastaRemove = teclado.next();
-                                if(escolhaPastaRemove.equals("Y")){
+                                if (escolhaPastaRemove.equals("Y")) {
                                     novaPasta.remove(i);
                                     System.out.println("Pasta Excluida com sucesso\n");
                                     stop = false;
                                     break;
-                                }else{
+                                } else {
                                     System.out.println("\nPasta não excluida !\n");
                                     stop = false;
                                     break;
@@ -72,7 +96,32 @@ public class Gdrive {
                     }
                     break;
                 case "2":
-                    
+                    if (novaPasta.isEmpty()) {
+                        System.out.println("Impossuivel apagar uma pasta inexistente");
+                        stop = false;
+                    } else {
+                        System.out.println("\nEscolha uma pasta, que deseje apagar alguma sub-pasta: \n");
+                        pesquisarPasta();
+                        escolhaPastaRemove = teclado.next();
+                        for (Pasta principal : novaPasta) {
+                            if (principal.nomePasta.equals(escolhaPastaRemove)) {
+                                System.out.println("\nInforme o nome da subPasta que deseja apagar :\n");
+                                escolhaPastaRemove = teclado.next();
+                                for (int i = 0; i < principal.novaSubPasta.size(); i++) {
+                                    if (principal.novaSubPasta.get(i).nomeSubpastas.equals(escolhaPastaRemove)) {
+                                        principal.novaSubPasta.remove(i);
+                                        System.out.println("\nPasta removida com sucesso !");
+                                        stop = false;
+                                        break;
+                                    } else {
+                                        System.out.println("\nPasta nao encontrada !");
+                                    }
+                                }
+                            } else {
+                                System.out.println("\nNao existe pasta com o nome informado !");
+                            }
+                        }
+                    }
                     break;
             }
         }
@@ -123,7 +172,7 @@ public class Gdrive {
                                 if (abrirPasta.nomePasta.equals(nomeConteudo)) {
                                     System.out.println("\nPasta: " + abrirPasta.nomePasta);
                                     if (abrirPasta.novoArquivo.isEmpty()) {
-                                        System.out.println("\nNao Existe arquivos dentro da pasta !");
+                                        System.out.println("\nNao Existe arquivos dentro da pasta: " + abrirPasta.nomePasta);
                                     } else {
                                         for (Arquivo arquivoPasta : abrirPasta.novoArquivo) {
                                             System.out.println(" -Arquivo da pasta: " + arquivoPasta.nomeArquivo + arquivoPasta.tipoArquivo + " Tamanho: " + arquivoPasta.tamanhoArquivo);
@@ -136,7 +185,7 @@ public class Gdrive {
                                         for (SubPastas subEncontrada : abrirPasta.novaSubPasta) {
                                             System.out.println(subEncontrada.nomeSubpastas);
                                             if (subEncontrada.novoArquivo.isEmpty()) {
-                                                System.out.println("\nNao existem arquivos dentro das sub-pastas\n");
+                                                System.out.println("\nNao existem arquivos dentro da sub-pasta\n" + subEncontrada.nomeSubpastas);
                                             } else {
                                                 for (Arquivo encontrado : subEncontrada.novoArquivo) {
                                                     System.out.println("  --Arquivo da sub-pasta: " + encontrado.nomeArquivo + encontrado.tipoArquivo + " Tamanho : " + encontrado.tamanhoArquivo);
@@ -152,22 +201,22 @@ public class Gdrive {
                             case "3":
                                 System.out.println("\nInforme o nome da sub-pasta que deseja : \n");
                                 String subPasta = teclado.next();
-                                if(abrirPasta.novaSubPasta.isEmpty()){
+                                if (abrirPasta.novaSubPasta.isEmpty()) {
                                     System.out.println("\nCrie uma sub-pasta primeiramente !\n");
-                                }else{
-                                    for(SubPastas criacaoArquivoSub : abrirPasta.novaSubPasta){
-                                       if(criacaoArquivoSub.nomeSubpastas.equals(subPasta)){
-                                           System.out.println("\nInforme o nome do arquivo que deseja criar: \n");
-                                           String nameNewArquivo = teclado.next();
-                                           System.out.println("\nInforme o tipo de arquivo, ex: .txt, .rar\n");
-                                           String tipoDeArquivo = teclado.next();
-                                           System.out.println("\nInforme o tamanho do arquivo (USE ,(virgula) ao inves de . (ponto) parvalores quebrados !)\n");
-                                           double tamanhoArquivo = teclado.nextFloat();
-                                           Arquivo newArquivo = new Arquivo(nameNewArquivo, tipoDeArquivo,tamanhoArquivo);
-                                           criacaoArquivoSub.novoArquivo.add(newArquivo);
-                                           System.out.println("\nArquivo criado com sucesso !\n");
-                                           break;
-                                       }
+                                } else {
+                                    for (SubPastas criacaoArquivoSub : abrirPasta.novaSubPasta) {
+                                        if (criacaoArquivoSub.nomeSubpastas.equals(subPasta)) {
+                                            System.out.println("\nInforme o nome do arquivo que deseja criar: \n");
+                                            String nameNewArquivo = teclado.next();
+                                            System.out.println("\nInforme o tipo de arquivo, ex: .txt, .rar\n");
+                                            String tipoDeArquivo = teclado.next();
+                                            System.out.println("\nInforme o tamanho do arquivo (USE ,(virgula) ao inves de . (ponto) parvalores quebrados !)\n");
+                                            double tamanhoArquivo = teclado.nextFloat();
+                                            Arquivo newArquivo = new Arquivo(nameNewArquivo, tipoDeArquivo, tamanhoArquivo);
+                                            criacaoArquivoSub.novoArquivo.add(newArquivo);
+                                            System.out.println("\nArquivo criado com sucesso !\n");
+                                            break;
+                                        }
                                     }
                                 }
                                 break;
--- gdrive/src/gdrive/GdriveMain.java
+++ gdrive/src/gdrive/GdriveMain.java
@@ -7,7 +7,7 @@ public class GdriveMain {
     public static void main(String[] args) {
         Gdrive drive = new Gdrive();
         Pasta newPasta;
-        String menu = "1) Criar Pasta\n2) Criar um arquivo \n3) Criar uma subPasta \n4) Excluir uma pasta";
+        String menu = "1) Criar Pasta\n2) Criar um arquivo ou pesquisar conteudo de uma pasta \n3) Criar uma subPasta \n4) Excluir uma pasta \n5) Verificar o tamanho de uma pasta";
         String escolha;
         String nomePasta;
         boolean stop = true;
@@ -15,7 +15,7 @@ public class GdriveMain {
         Scanner selecaoDeOpcao = new Scanner(System.in);
 
         while (stop != false) {
-            System.out.println("Informe a opcao que deseja: \n\n");
+            System.out.println("\nInforme a opcao que deseja: \n\n");
             System.out.println(menu);
             escolha = teclado.next();
             switch (escolha) {
@@ -28,19 +28,22 @@ public class GdriveMain {
                     break;
                     
                 case "2":
-                    System.out.println("Escreva o nome da pasta que deseja adicionar um arquivo (ignore os numeros iniciais)");
+                    System.out.println("Escreva o nome da pasta que deseja adicionar um arquivo (ignore os numeros iniciais)\nCaso nao exista nenhuma pasta, abaixo, que nao existem pastas no drive");
                     drive.pesquisarPasta();
                     String opcaoPasta = selecaoDeOpcao.next();
                     drive.abrirPasta(opcaoPasta);
                     break;
                 case "3":
-                    System.out.println("Escreva o nome da pasta que deseja adicionar uma nova pasta (ignote os numeros iniciais)");
+                    System.out.println("Escreva o nome da pasta que deseja adicionar uma nova pasta (ignore os numeros iniciais)\nCaso nao exista nenhuma pasta, abaixo, que nao existem pastas no drive");
                     drive.pesquisarPasta();
                     String nomePastaPesquisa = selecaoDeOpcao.next();
                     drive.adicionarSubPastas(nomePastaPesquisa);
                     break;
                 case "4":
                     drive.excluirPasta();
+                    break;
+                case "5":
+                    System.out.println("O tamanho da pasta eh: " + drive.calcTamanhoPast() + "\n(Quando a pasta estiver vazia, o tamanho dela eh 1.0)");
                     break;
             }
         }
